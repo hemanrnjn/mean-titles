3aaa8da8bae348cc01f8eda2aa4a66c255112f49
Himanshu <himanshurnjn04@gmail.com>
2020-02-22 17:17:08 +0530

    First POC

--- script.js
+++ script.js
@@ -2,43 +2,59 @@ chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
   changeSubtitlesStyle(message.vPos, message.fSize, message.fColor);
 });
 
+function getMeaning(video, vPos) {
+    let val = "";
+    const subtitles = document.querySelector(".player-timedtext");
+    if (subtitles && video && video.paused) {
+        subtitles.style.bottom = vPos + "px";
+
+        // .player-timedtext > .player-timedtext-container [0]
+        const firstChildContainer = subtitles.firstChild;
+        if (subtitles.firstChild != null && val === "") {
+            // console.log("mila", subtitles.firstChild.innerText)
+            val = subtitles.firstChild.innerText;
+            // console.log(val);
+            var request = new XMLHttpRequest();
+            request.open('GET', "http://localhost:8000/api/meaning/" + val.split(" ")[0], true);
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+            request.setRequestHeader('Accept', 'application/json');
+            request.send();
+
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    let resp = JSON.parse(request.responseText);
+                    if (resp.includes("Error 404")) {
+                        console.log("Invalid Word")
+                    } else {
+                        let obj = JSON.parse(resp);
+                        console.log(obj.definitions[0].definition);
+                    }
+                }
+            };
+        }
+    }
+}
+
 changeSubtitlesStyle = (vPos, fSize, fColor) => {
   console.log("%cnetflix-subtitles-styler : observer is working... ", "color: red;");
-  var val = ""
-  callback = () => {
-    // .player-timedText
-    const subtitles = document.querySelector(".player-timedtext");
-    if (subtitles) {
-      subtitles.style.bottom = vPos + "px";
-
-      // .player-timedtext > .player-timedtext-container [0]
-      const firstChildContainer = subtitles.firstChild;
-      if (subtitles.firstChild != null && val == "") {
-        console.log("mila", subtitles.firstChild.innerText)
-          val = subtitles.firstChild.innerText
-          console.log(val)
-          var request = new XMLHttpRequest();
-          request.open('GET', "https://owlbot.info/api/v4/dictionary/" + val.split(" ")[0], true);
-          request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-          request.setRequestHeader('Authorization', 'Token 23db56a1aace2a60d425f5456265e8d988819728');
-          request.setRequestHeader('Accept', 'application/json');
-          request.setRequestHeader('Access-Control-Allow-Origin', '*');
-          request.setRequestHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS, PUT, DELETE');
-          request.send();
-
-          request.onreadystatechange = function () {
-              if (request.readyState === 4) {
-                console.log(request.responseText);
-              }
-          };
-        }
+  let callback = () => {
+      const video = document.querySelector("video:first-of-type");
+      // .player-timedText
+      getMeaning(video, vPos);
+      if (video) {
+          video.addEventListener("onpause", function(e) {
+              console.log(e);
+              getMeaning(video, vPos)
+          });
       }
   };
 
+
+
   const observer = new MutationObserver(callback);
   observer.observe(document.body, {
-    subtree: true,
-    attributes: false,
-    childList: true
+      subtree: true,
+      attributes: false,
+      childList: true
   });
 };
